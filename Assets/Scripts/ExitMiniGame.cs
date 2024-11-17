b32f647516230ef9e473ec93f4383f4303f33c27
Sviatoslav <sama25920@gmail.com>
2024-11-17 23:45:26 +0200

    Add Card MiniGame

---
+++ Assets/Scripts/ExitMiniGame.cs
@@ -0,0 +1,23 @@
+﻿using Naninovel;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitMiniGame : MonoBehaviour
+{
+    public void Exit()
+    {
+        var inputManager = Engine.GetService<IInputManager>();
+        inputManager.ProcessInput = true;
+
+        var scriptPlayer = Engine.GetService<IScriptPlayer>();
+        scriptPlayer.PreloadAndPlayAsync("Map").Forget();
+
+        var hidePrinter = new Naninovel.Commands.HidePrinter();
+        hidePrinter.ExecuteAsync().Forget();
+
+        var naniCamera = Engine.GetService<ICameraManager>().Camera;
+        naniCamera.enabled = true;
+
+        SceneManager.LoadScene("NaniScene");
+    }
+}
